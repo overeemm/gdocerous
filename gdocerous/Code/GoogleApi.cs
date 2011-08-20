a71027723caa46185890b8b5908ca7c06c1c97cb
unknown <michiel@.smallbusiness.local>
2011-08-20 16:54:39 +0200

    initial drop of code, google docs interfacing working

---
+++ gdocerous/Code/GoogleApi.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Google.GData.Documents;
+using Google.GData.Client;
+using Google.Documents;
+using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib;
+
+namespace gdocerous.Code
+{
+    public class GoogleApi
+    {
+        private string m_token;
+        private DocumentsRequest m_request;
+
+        public GoogleApi(string token)
+        {
+            m_token = token;
+            m_request = new DocumentsRequest(new RequestSettings(MvcApplication.APPNAME, Token));
+        }
+
+        public GoogleApi()
+        {
+        }
+
+        private string Token { get { return m_token; } }
+
+        public string GetAuthUrl()
+        {
+            return AuthSubUtil.getRequestUrl("http://localhost:27617/Authenticate", "https://docs.google.com/feeds/ https://mail.google.com/", false, true);
+        }
+
+        public string GetSessionToken(string requesttoken)
+        {
+            return AuthSubUtil.exchangeForSessionToken(requesttoken, null);
+        }
+
+        public Feed<Document> GetFolders()
+        {
+            Feed<Document> folders = m_request.GetFolders();
+            return folders;
+        }
+
+        public Document GetFolder(string folder)
+        {
+            Feed<Document> folders = GetFolders();
+
+            foreach (Document d in folders.Entries)
+                if (d.Title == folder)
+                    return d;
+            return null;
+        }
+
+        public Feed<Document> GetDocuments(string folder)
+        {
+            return m_request.GetFolderContent(GetFolder(folder));
+        }
+
+        public Document GetDocument(string folder, string document)
+        {
+            Feed<Document> documents = m_request.GetFolderContent(GetFolder(folder));
+
+            foreach (Document d in documents.Entries)
+                if (d.Title == document)
+                    return d;
+            return null;
+        }
+
+        /// <summary>
+        /// Get the zip-html version, extract the html file and present it (to review, not to edit).
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <returns></returns>
+        public string GetHtmlContent(Document doc)
+        {
+            using (Stream str = GetDocExportStream(doc, Document.DownloadType.zip))
+            using (ZipInputStream zipInputStream = new ZipInputStream(str))
+            {
+                ZipEntry zipEntry = zipInputStream.GetNextEntry();
+                while (zipEntry != null)
+                {
+                    String entryFileName = zipEntry.Name;
+                    if (entryFileName.ToLowerInvariant().EndsWith(".html"))
+                    {
+                        byte[] buffer = new byte[4096];
+                        using (MemoryStream mstream = new MemoryStream())
+                        using (StreamReader reader = new StreamReader(mstream))
+                        {
+                            StreamUtils.Copy(zipInputStream, mstream, buffer);
+                            mstream.Position = 0;
+                            return reader.ReadToEnd();
+                        }
+                    }
+                    zipEntry = zipInputStream.GetNextEntry();
+                }
+
+                return "no HTML file found!";
+            }
+        }
+
+        /// <summary>
+        /// Instead of DocumentsRequest.Download ; that function keeps returning a 404.
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="downloadtype"></param>
+        /// <returns></returns>
+        private Stream GetDocExportStream(Document doc, Document.DownloadType downloadtype)
+        {
+            string format = downloadtype.ToString();
+            string url = doc.DocumentEntry.Content.AbsoluteUri + "&exportFormat=" + format + "&format=" + format;
+            return m_request.Service.Query(new Uri(url));
+        }
+
+        public Stream GetDocumentContent(string folder, string document)
+        {
+            return GetDocExportStream(GetDocument(folder, document), Document.DownloadType.zip);
+        }
+    }
+}
