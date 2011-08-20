131a24ecaab0af18dc956db24515cf3a87d525c7
unknown <michiel@.smallbusiness.local>
2011-08-20 16:54:39 +0200

    initial drop of code, google docs interfacing working

---
+++ gdocerous/Controllers/DefaultController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using gdocerous.Code;
+
+namespace gdocerous.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public string GoogleAuthToken { get { return Session["token"] as string; } set { Session["token"] = value; } }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return Redirect(new GoogleApi().GetAuthUrl());
+        }
+
+        public ActionResult Authenticate()
+        {
+            GoogleAuthToken = new GoogleApi().GetSessionToken((String)Request["token"]);
+            return RedirectToAction("Categories");
+        }
+
+        public ActionResult Categories()
+        {
+            ViewBag.Folders = new GoogleApi(GoogleAuthToken).GetFolders();
+            return View();
+        }
+
+        public ActionResult Documents(string folder)
+        {
+            ViewBag.Folder = folder;
+            ViewBag.Documents = new GoogleApi(GoogleAuthToken).GetDocuments(folder);
+            return View();
+        }
+
+        public ActionResult Document(string folder, string document)
+        {
+            GoogleApi docs = new GoogleApi(GoogleAuthToken);
+
+            ViewBag.Folder = folder;
+            ViewBag.Document = docs.GetDocument(folder, document);
+            ViewBag.DocumentHtml = docs.GetHtmlContent(ViewBag.Document);
+            return View();
+        }
+
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send(string folder, string document, string tags)
+        {
+            using (Posterous post = new Posterous(new GoogleApi(GoogleAuthToken).GetDocumentContent(folder, document), document))
+            {
+                post.Send(tags);
+            }
+
+            return RedirectToAction("Send");
+        }
+    }
+}
---
+++ gdocerous/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace gdocerous
+{
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public const string APPNAME = "gDocerous";
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("documents", "{action}/{folder}/{document}", new { controller = "Default", action = "Index", folder = UrlParameter.Optional, document = UrlParameter.Optional });
+            routes.MapRoute("folders", "{action}/{folder}", new { controller = "Default", action = "Index", folder = UrlParameter.Optional } );
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
