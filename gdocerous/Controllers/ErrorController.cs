c16ae82570335df239567b001a79afbd241e4b85
Michiel Overeem <overeemm@gmail.com>
2011-08-21 19:18:55 +0200

    Added missing files

---
+++ gdocerous/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gdocerous.Controllers
+{
+    public class ErrorController : Controller
+    {
+
+        public ActionResult General(Exception exception)
+        {
+            return View("Exception", exception);
+        }
+
+        public ActionResult Http404()
+        {
+            return View("404");
+        }
+
+    }
+}
