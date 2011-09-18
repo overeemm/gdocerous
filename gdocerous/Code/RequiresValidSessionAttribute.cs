7923ce6eb3ac18772d254877bb48a1be6fc234f7
Michiel Overeem <overeemm@gmail.com>
2011-09-18 18:55:02 +0200

    missing code file

---
+++ gdocerous/Code/RequiresValidSessionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gdocerous.Code
+{
+    public class RequiresValidSessionAttribute : ActionFilterAttribute
+    {
+        public RequiresValidSessionAttribute()
+        {
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Session.IsValidSession())
+            {
+                filterContext.Result = new RedirectResult(new UrlHelper(filterContext.RequestContext).Action("Index", "Default"));
+            }
+        }
+    }
+}
